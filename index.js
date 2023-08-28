28a0edb0c8adc59fed2af38f30f22ed2dba0d5b3
Mukesh <mouryamukesh8692@gmail.com>
2023-08-28 22:04:05 +0530

    updated cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "https://mydiarymern.netlify.app", credentials: true }));
 const port = 5000;
 
 app.get("/", (req, res) => {
